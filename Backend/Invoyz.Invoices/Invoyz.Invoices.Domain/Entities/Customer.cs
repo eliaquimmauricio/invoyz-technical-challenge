78b44fc813cbd1a6666f5f6a14a4345d1bd20d51
Eliaquim Mauricio <ext_ems@biometric.dk>
2026-03-14 11:15:49 +0000

    Make Customer Email and Address required; compute InvoiceLine totals

--- Backend/Invoyz.Invoices/Invoyz.Invoices.Domain/Entities/Customer.cs
+++ Backend/Invoyz.Invoices/Invoyz.Invoices.Domain/Entities/Customer.cs
@@ -4,8 +4,8 @@ namespace Invoyz.Invoices.Domain.Entities
     {
         public required string Name { get; set; }
         public required string VatNumber { get; set; }
-        public string? Email { get; set; }
-        public string? Address { get; set; }        
+        public required string Email { get; set; }
+        public required string Address { get; set; }        
         public ICollection<Invoice>? Invoices { get; set; }
     }
 }
--- Backend/Invoyz.Invoices/Invoyz.Invoices.Domain/Entities/InvoiceLine.cs
+++ Backend/Invoyz.Invoices/Invoyz.Invoices.Domain/Entities/InvoiceLine.cs
@@ -7,8 +7,8 @@ namespace Invoyz.Invoices.Domain.Entities
         public required int Quantity { get; set; }
         public required decimal UnitPrice { get; set; }
         public required decimal TaxRate { get; set; }
-        public required decimal LineTotal { get; set; }
-        public required decimal LineTax { get; set; }
+        public decimal LineTotal => Quantity * UnitPrice;
+        public decimal LineTax => LineTotal * (TaxRate / 100);
         public required Invoice Invoice { get; set; }
         public required Product Product { get; set; }
     }
